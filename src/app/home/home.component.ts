d858565df0539953d9ec4572985143017794ba54
CindyKatoni <katmuema@gmail.com>
2021-05-19 23:39:46 +0300

    import output and eventemitter in search

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -38,6 +38,7 @@ export class HomeComponent implements OnInit {
 
 
   ngOnInit(): void {
+    this.getUsers("CindyKatoni");
   }
 
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  userName:string;
+  @Output() searchEmitter = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  search() {
+    this.searchEmitter.emit(this.userName);
+    this.userName = '';
+  }  
 
 }
